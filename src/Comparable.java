6cc2fb8f7443604c5252e23afe5ba7b2075415ac
PuddingPudding <hwsh70119@gmail.com>
2016-12-24 15:56:08 +0800

    新增介面 Comparable，請其他類別自行實作判斷大小的方法

---
+++ src/Comparable.java
@@ -0,0 +1,8 @@
+public interface Comparable 
+{
+	public static final int ERROR = -2;
+	public static final int SMALLER = -1;
+	public static final int SAME = 0;
+	public static final int BIGGER = 1;
+	public int compareTo(Comparable input);
+}
